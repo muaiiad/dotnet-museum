9040386d7cf199ac4515d433e5068095015186f9
Moayad Ali <108472902+muaiiad@users.noreply.github.com>
2025-08-13 19:53:17 +0300

    add DbContext

---
+++ dotnet-museum/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_museum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_museum.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Artifact> Artifacts { get; set; }
+    public DbSet<Gallery> Galleries { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+        optionsBuilder.UseSqlServer(
+            "Server=(localdb)\\MSSQLLocalDB;Database=DotnetMuseum;Trusted_Connection=True;"
+        );
+    }
+    
+}
