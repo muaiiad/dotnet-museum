8d32fa84b28ddda3ec36b74985b2cb80e15188f2
Moayad Ali <108472902+muaiiad@users.noreply.github.com>
2025-08-13 17:54:32 +0300

    add artifact and gallery classes

---
+++ dotnet-museum/Models/Artifact.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnet_museum.Models;
+
+public class Artifact
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string Origin { get; set; }
+    public required string Period { get; set; }
+    public required string Civilization { get; set; }
+    public required string GalleryId {get;set;}
+    
+    
+}
---
+++ dotnet-museum/Models/Gallery.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_museum.Models;
+
+public class Gallery
+{
+    public int GalleryId { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public IList<Artifact> Artifacts { get; set; }
+}
