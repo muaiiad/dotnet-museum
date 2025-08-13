dcb5d6087e561aa93d2cf4831baa6aaf04efab9e
Menna Hisham <mennahisham2050@gmail.com>
2025-08-14 01:50:10 +0300

    Test

---
+++ dotnet-museum/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_museum.Controllers;
+
+public class CompanyController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
