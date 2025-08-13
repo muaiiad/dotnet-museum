cebb2187e1a1b2768e5967ab049c4ffff59e3b49
Moayad Ali <108472902+muaiiad@users.noreply.github.com>
2025-08-13 17:57:51 +0300

    add employee class

---
+++ dotnet-museum/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnet_museum.Models;
+
+public class Employee
+{
+    public required int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Title { get; set; }
+    public required string Department { get; set; }
+    public required float Salary { get; set; }
+    
+}
