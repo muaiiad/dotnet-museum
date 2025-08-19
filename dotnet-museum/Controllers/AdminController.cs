efd656c317b4197cc85fe38e2dd5b26c81113c70
Moayad Ali <108472902+muaiiad@users.noreply.github.com>
2025-08-20 01:56:05 +0300

    add admin dashboard

---
+++ dotnet-museum/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_museum.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
